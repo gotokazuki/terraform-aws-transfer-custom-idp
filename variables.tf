11b7debfad45e78f8a176fca9f1e832ce6e1e965
gotooooo <k.goto.123@gmail.com>
2020-06-22 13:07:45 +0900

    description

--- variables.tf
+++ variables.tf
@@ -1,11 +1,52 @@
-variable "s3_bucket_arn" {}
-variable "iam_role_apigw_logging_arn" {}
-variable "iam_role_lambda_basic_execution_arn" {}
-variable "name_prefix" { default = "" }
-variable "lambda_runtime" { default = "nodejs12.x" }
-variable "lambda_get_user_config_function_source_dir" { default = "use_default_function" }
-variable "lambda_get_user_config_function_output_path" { default = "use_default_function" }
-variable "user_name" { default = "myuser" }
-variable "user_home_directory" { default = "/" }
-variable "user_password" { default = "MySuperSecretPassword" }
-variable "user_public_key_1" { default = "ssh-rsa myrsapubkey" }
+variable "s3_bucket_arn" {
+  description = "The arn of the S3 bucket used for the Transfer Server"
+  type        = string
+}
+variable "iam_role_apigw_logging_arn" {
+  description = "The arn of the IAM Role used by the API Gateway for logging"
+  type        = string
+}
+variable "iam_role_lambda_basic_execution_arn" {
+  description = "The arn of the IAM Role used by the Lambda for executing lambda functions"
+  type        = string
+}
+variable "name_prefix" {
+  description = "String to use as a prefix for the object names"
+  type        = string
+  default     = ""
+}
+variable "lambda_runtime" {
+  description = "lambda runtime"
+  type        = string
+  default     = "nodejs12.x"
+}
+variable "lambda_get_user_config_function_source_dir" {
+  description = "The source dir of the lambda function"
+  type        = string
+  default     = "use_default_function"
+}
+variable "lambda_get_user_config_function_output_path" {
+  description = "The output path of the lambda function"
+  type        = string
+  default     = "use_default_function"
+}
+variable "user_name" {
+  description = "userName in lambda function"
+  type        = string
+  default     = "myuser"
+}
+variable "user_home_directory" {
+  description = "userHomeDirectory in lambda function"
+  type        = string
+  default     = "/"
+}
+variable "user_password" {
+  description = "userPassword in lambda function"
+  type        = string
+  default     = "MySuperSecretPassword"
+}
+variable "user_public_key_1" {
+  description = "userPublicKey1 in lambda functio"
+  type        = string
+  default     = "ssh-rsa myrsapubkey"
+}
